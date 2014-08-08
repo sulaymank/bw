bc971bffa176d16769a661dcf9981753c94df038
Sulayman Khan <sulaymank@users.noreply.github.com>
2014-08-08 10:16:04 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+  $dbhost = 'localhost';
+  $dbname = 'brightworks';
+  $dbusername = 'brightworks';
+  $dbuserpass = 'frTZwHbQWRy493fr';
+  $staging = true;	//change to true when site is in production
+
+  $sitename = "Bayer HealthCare Consent";
+  
+  $title = "Bayer HealthCare Consent";
+  $description = "Bayer HealthCare Consent";
+  $keywords = "";
+
+?>
